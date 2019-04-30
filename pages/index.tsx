fb365c263740d9dfc4e5a0c9a0e24ee2aeb04e31
Eugenio Depalo <eugeniodepalo@gmail.com>
2019-04-30 10:56:12 +0100

    Add regions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,13 @@
+import Head from 'next/head';
+
 function Index() {
+  return (
+    <div>
+      <Head>
+        <title>Eugenio Depalo - CV</title>
+      </Head>
+    </div>
+  )
   return <div>Welcome to Next.js!</div>;
 }
 
