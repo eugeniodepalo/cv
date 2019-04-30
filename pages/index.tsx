5879c99014ba57a65cfb5b1ea6a67c3d0277a3c2
Eugenio Depalo <eugeniodepalo@gmail.com>
2019-04-30 20:07:36 +0100

    Remove page createStore

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,14 @@
 import createContainer from 'cv/createContainer'
-import createStore from './index/createStore'
+import cvCreateStore from 'cv/createStore'
+import reducer from './index/reducer'
 import Page from './index/components/Page'
+
+const initialState = {
+  count: 0
+}
+
+const createStore = (state = initialState) => {
+  return cvCreateStore(reducer, state)
+}
 
 export default createContainer(createStore, Page)
