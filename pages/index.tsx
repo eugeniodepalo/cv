da091b4a4ec3acad9191c6eaccebe8714b1a9906
Eugenio Depalo <eugeniodepalo@gmail.com>
2019-07-17 22:54:34 +0100

    Move index outside pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import createContainer from '~/createContainer'
 import rootCreateStore from '~/createStore'
-import reducer, { State } from './index/reducer'
-import Page from './index/components/Page'
-import saga from './index/saga'
+import reducer, { State } from '~/index/reducer'
+import Page from '~/index/components/Page'
+import saga from '~/index/saga'
 
 export default createContainer<State>((state) => rootCreateStore(reducer, state, saga), Page)
